9d316da3685ef6a298e38c6adea6277c171de724
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-14 05:34:21 -0700

    lower alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alpha;
+	/*for loop*/
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar (alpha);
+	putchar ('\n');
+	return (0);
+}
