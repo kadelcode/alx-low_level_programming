a251768308160534d06a1723938aa25fe30e508a
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-14 08:04:59 -0700

    print alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha == 'q')
+			continue;
+		if (alpha == 'e')
+			continue;
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
