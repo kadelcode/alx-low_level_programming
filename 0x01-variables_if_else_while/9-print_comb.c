9fdab4faca3db9b8f9532c20ae277402ddf1a9bc
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-14 11:26:46 -0700

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* variable definition */
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar(' ');
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
