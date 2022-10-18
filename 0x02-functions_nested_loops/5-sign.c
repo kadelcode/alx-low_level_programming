3d9a78c8521cb23f5646d632cf693b7603beb5cd
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-18 07:16:01 -0700

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_sign - print the sign of an integer
+* @n: the parameter that accept a number
+* Return: if n > 0 return 1, if n = 0 return 0, and if n < 0 return -1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	if (n == 0)
+	{
+		_putchar(0 + '0');
+		return (0);
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
