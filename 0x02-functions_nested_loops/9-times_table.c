84153309576d6cc40905d9e458f138a3b285227e
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-18 13:42:58 -0700

    times table(9)

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,19 +6,22 @@
 void times_table(void)
 {
 	/* declaring variables */
-	int i, j;
+	int i, j, mul;
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar(0 + '0');
+		_putchar(0);
 		for (j = 1; j <= 9; j++)
 		{
 			_putchar(',');
 			_putchar(' ');
-			_putchar((i * j) + '0');
-			j++;
+			mul = i * j;
+			if (mul <= 9)
+				_putchar(' ');
+			else
+				_putchar((mul / 10) + '0');
+		_putchar((mul % 10) + '0');
 		}
 		_putchar('\n');
-		i++;
 	}
 }
