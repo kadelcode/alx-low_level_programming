93682b603ec48f208a424109078dcfedfb6f2def
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-18 05:28:04 -0700

    lower case letters

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,7 @@
 #include "main.h"
 /**
-* main - calls the function print_alphabet()
-* Description: The print_alphabet() prints alphabet from a to z in lower case
-* print_alphabet - prints lowercase alphabets
-* Return: Always 0
+* print_alphabet - print lower case alphabets
 */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 
 void print_alphabet(void)
 {
