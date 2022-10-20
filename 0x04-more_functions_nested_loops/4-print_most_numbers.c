47e1a18587c15a088849633ba1ed795e7fe5894f
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-20 07:39:59 -0700

    print 0-9 not 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_most_numbers - print numbers from 0 to 9, except 2 and 4
+*/
+
+void print_most_numbers(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		if (c == 50)
+			continue;
+		if (c == 52)
+			continue;
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n')
+}
+
