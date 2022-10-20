ba196bd11dcf3babe83d181660ca4f8b5762793a
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-20 03:38:38 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_numbers - prints numbers from 0 to 9
+*/
+void print_numbers(void)
+{
+	char c;
+
+	while ((c >= 48) && (c <= 57))
+	{
+		_putchar(c);
+	}
+	_putchar('\n')
+}
