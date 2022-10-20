ee445fee9019824264a760ba831c0d407de8394e
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-20 14:48:48 -0700

    Hotfix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,7 +12,7 @@ void print_triangle(int size)
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for (j = size - 1; j > 0; j--)
+			for ((j = size - 1); j > 0; j--)
 				_putchar(' ');
 			for (j = 0; j < i; j++)
 				_putchar('#');
