c9e02324b43813feebf58d2c04e5ee296c35b379
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-20 09:23:55 -0700

    print 0-14, 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* more_numbers - prints 0 to 14, 10 times
+*/
+void more_numbers(void)
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		int j = 0;
+
+		while (j <= 14)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			else
+				_putchar(j + '0');
+			j++;
+		}
+		i++
+		_putchar('\n');
+	}
+}
