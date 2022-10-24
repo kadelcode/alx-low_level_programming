8d0f7527e407af50eb4b442fb1d36132ee55d443
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-24 09:24:25 -0700

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,7 +6,26 @@
 
 void print_rev(char *s)
 {
-	while (*s)
-		_putchar(*s--);
+	int len = 0, i = 0;
+
+	len = _strlen(s);
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
+}
+
+/**
+* _strlen - prints length of string
+* @s: parameter
+* Return: return the string len
+*/
+int _strlen(char *s)
+{
+	int count, j = 0;
+
+	j = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+		j++;
+	return (j);
 }
