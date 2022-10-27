dc51f70c1c43a31776622d6a7b09950878d3f4f8
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-27 12:29:07 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,22 +9,22 @@ char *rot13(char *str)
 {
 	int i, j;
 
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+	char alphabet[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
 	'J', 'K', 'L', 'M', 'N', '0', 'P',
 	'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
 	'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
 	'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-	char rot13key[52] = {'N', '0', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+	char rot13key[] = {'N', '0', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
 	'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 	'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
 	'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 
-	while (str[++i])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < 52; j++)
+		for (j = 0; j < 53; j++)
 		{
-			if (str[i] == alphabet[j])
+			if (alphabet[j] == str[i])
 			{
 				str[i] = rot13key[j];
 				break;
