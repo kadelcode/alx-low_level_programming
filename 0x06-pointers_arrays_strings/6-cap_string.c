361a3828ce860ad0eaa1eaa4400b21aa04cba249
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-27 11:50:14 -0700

    making changes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,24 +9,28 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[++i])
+	while (str[i] != '\0')
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
+		if (str[0] >= 97 && str[0] <= 122)
+			str[0] = str[0] - 32;
 
-		if (str[i - 1] == ' ' ||
-		str[i - 1] == '\t' ||
-		str[i - 1] == ',' ||
-		str[i - 1] == ';' ||
-		str[i - 1] == '.' ||
-		str[i - 1] == '!' ||
-		str[i - 1] == '?' ||
-		str[i - 1] == '"' ||
-		str[i - 1] == '(' ||
-		str[i - 1] == ')' ||
-		str[i - 1] == '{' ||
-		str[i - 1] == '}')
-			str[i] -= 32;
+		if (str[i] == ' ' ||
+		str[i] == '\t' ||
+		str[i] == ',' ||
+		str[i] == ';' ||
+		str[i] == '.' ||
+		str[i] == '!' ||
+		str[i] == '?' ||
+		str[i] == '"' ||
+		str[i] == '(' ||
+		str[i] == ')' ||
+		str[i] == '{' ||
+		str[i] == '}')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				str[i + 1] -= 32;
+		}
+		i++;
 	}
 	return (str);
 }
