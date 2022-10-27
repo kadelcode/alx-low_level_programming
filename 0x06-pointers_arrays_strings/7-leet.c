fefeddde4fe06e56dff382d4dafb7e5bbe324012
Ayodele Kadiri <kadelcode@gmail.com>
2022-10-27 12:04:47 -0700

    leetcode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,17 +7,24 @@
 
 char *leet(char *str)
 {
-	int i = 0;
+	int l_count, s_count;
 
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	char leetLets[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
 
-	while (str[++i])
+	s_count = 0;
+	while (str[s_count] != '\0')
 	{
-		for (int j = 0; j <= 7; j++)
+		l_count = 0;
+		while (l_count < 10)
 		{
-			if (str[i] == leet[j] || str[i] - 32 == leet[j])
-				str[i] = j + '0';
+			if (leetLets[l_count] == str[s_count])
+			{
+				str[s_count] = leetNums[l_count];
+			}
+			l_count++;
 		}
+		s_count++;
 	}
 	return (str);
 }
