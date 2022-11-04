1406294bb6fe60814fb2ab7563844a4b05dbf79e
Ayodele Kadiri <kadelcode@gmail.com>
2022-11-04 14:16:56 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - print the number of arguments
+* @argc: The size of argv array
+* @argv: an array of size arg c
+*/
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
