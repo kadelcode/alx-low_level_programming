efebb9bf67a67c032b5a9167a3dfb87bf508084e
Ayodele Kadiri <kadelcode@gmail.com>
2022-11-04 14:22:23 -0700

    update code

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,9 +3,10 @@
 * main - print the number of arguments
 * @argc: The size of argv array
 * @argv: an array of size arg c
+* Return: Always 0
 */
 int main(int argc, char **argv)
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
