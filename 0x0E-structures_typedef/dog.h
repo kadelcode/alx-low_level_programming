b6426668af7d11ed784c9e8e8d66d012a1f22946
Ayodele Kadiri <kadelcode@gmail.com>
2022-11-14 08:44:24 -0800

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - a dog structure
+* @name: name of dog
+* @age: age of dog
+* @owner: the owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
