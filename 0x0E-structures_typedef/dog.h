d795d87d468a00e9af6d3a18d2a66bb3085695fd
Ayodele Kadiri <kadelcode@gmail.com>
2022-11-14 12:35:17 -0800

    new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,6 +6,8 @@ struct dog *d;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/* new type of struct dog */
+typedef struct dog dog_t;
 /**
 * struct dog - a dog structure
 * @name: name of dog
