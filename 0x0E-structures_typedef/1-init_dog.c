9a13531f47db68fe4907772af388cacf771be086
Ayodele Kadiri <kadelcode@gmail.com>
2022-11-14 09:39:12 -0800

    initializing dog param

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+* init_dog - initializes a variable of type struct
+* @d: pointer to struct dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = &my_dog;
+	(*d).name = "Poppy";
+	(*d).age = 3.5;
+	d->owner = "Bob";
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
+/* function prototypes */
+struct dog my_dog;
+struct dog *d;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 /**
 * struct dog - a dog structure
 * @name: name of dog
