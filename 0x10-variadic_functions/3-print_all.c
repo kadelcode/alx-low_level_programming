2263be38014b11bbe98753fa57b16ec2aa32a5b1
Ayodele Kadiri <kadelcode@gmail.com>
2022-11-17 11:08:54 -0800

    update code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -37,6 +37,8 @@ void print_all(const char * const format, ...)
 					printf("(nil)");
 					break;
 				}
+				printf("%s", string);
+				break;
 		}
 		if ((format[i] == 'c' || format[i] || format[i] == 'f' || format[i] == 's')
 		&& format[(i + 1)] != '\0')
