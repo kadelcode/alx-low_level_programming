d4cd69a3cf7c252075194bf59525fa3cb11d57f9
Ayodele Kadiri <kadelcode@gmail.com>
2022-11-17 08:05:14 -0800

    cod fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (i < n - 1)
 			if (separator)
-				printf("%s", separato);
+				printf("%s", separator);
 	}
 
 	printf("\n");
