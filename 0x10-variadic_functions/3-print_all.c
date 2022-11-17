ffc2bf7cd48abc0bb76f8fc5fe5c19741cf39ef4
Ayodele Kadiri <kadelcode@gmail.com>
2022-11-17 07:50:36 -0800

    prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include "variadic_functions.h"
+
+/**
+* print_all - prints anything
+* @format: format to print a value
+*/
+
+void print_all(const char * const format, ...)
+{
+	va_list vl;
+	int i;
+	char *string;
+
+	i = 0;
+
+	va_start(vl, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'i':
+				printf("%i", va_arg(vl, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(vl, double));
+				break;
+			case 'c':
+				printf("%c", (char) va_arg(vl, int));
+				break;
+			case 's':
+				string = va_arg(v1, char *);
+				if (string == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+		}
+		if ((format[i] == 'c' || format[i] || format[i] == 'f' || format[i] == 's')
+		&& format[(i + 1)] != '\0')
+			prinf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(vl);
+}
