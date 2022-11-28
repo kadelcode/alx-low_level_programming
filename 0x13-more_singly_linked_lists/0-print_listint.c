9b0bbc18c591f9e0bdb0a0ae6140e3c6ce4f0467
Ayodele Kadiri <kadelcode@gmail.com>
2022-11-28 05:45:28 -0800

    betty fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,7 +9,7 @@ size_t print_listint(const listint_t *h)
 	const listint_t *cursor = h;
 	size_t count = 0;
 
-	while(cursor != NULL)
+	while (cursor != NULL)
 	{
 		printf("%d\n", cursor->n);
 		count += 1;
