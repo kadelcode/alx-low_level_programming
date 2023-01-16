a6f172dd162c8f3d2fa20d755575e654208a35ed
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-15 23:30:26 -0800

    string length

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strlen - returns the length of a string
+* @s: string parameter
+* Return: the length of string
+*/
+int _strlen(char *s)
+{
+	int i, count;
+
+	count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	return (count);
+}
